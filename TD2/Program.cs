462908b66988c53cc755e14bc5a8c66d587f7402
Aabzyx <77335648+Aabzyx@users.noreply.github.com>
2021-02-13 09:14:10 +0100

    PremiereVersion

---
+++ TD2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TD2
+{
+    class Program
+    {
+        static void Main()
+        {
+            string fileName = "./Test001.bmp";
+            MyImage image = new MyImage(fileName);
+            Console.WriteLine(image.toString());
+            Console.WriteLine(image.AffichageMatricePixel());
+            image.From_Image_To_File("./Sortie.bmp");
+            Console.ReadKey(true);
+        }
+    }
+}
